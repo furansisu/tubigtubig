97d2f071722baa443760a290a9f2c25f63033c62
WIN10X64-TRIAL <francispasana@gmail.com>
2022-06-21 19:44:45 +0800

    Tilemap test

--- Player.gd
+++ Player.gd
@@ -3,10 +3,10 @@ extends KinematicBody2D
 # Called when the node enters the scene tree for the first time.
 
 var velocity = Vector2.ZERO
-var default_acc = 500
+export var default_acc = 400
 var acc = default_acc
-var maxspeed = 100
-var friction = acc/(maxspeed)
+export var maxspeed = 100
+var friction = (acc/maxspeed)
 var motion = Vector2.ZERO
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,13 +27,16 @@ func _physics_process(delta):
 	# calculation
 	velocity += motion * acc * delta
 	velocity -= velocity * friction * delta
+	
 
 	
 	# moving other bodies
 	for i in get_slide_count():
 		var collision = get_slide_collision(i)
-		collision.collider.move_and_slide(-collision.normal * friction)
-		velocity = velocity/2
+		if collision.collider is KinematicBody2D:
+			collision.collider.move_and_slide(-collision.normal * friction)
+			velocity = velocity/2
+
 		
 	# run test
 	if Input.is_action_pressed("run"):
