b091ef0ca181351917534e9eb14a1f25cb14e5de
WIN10X64-TRIAL <francispasana@gmail.com>
2023-07-07 18:53:02 +0800

    Godot 4.1 update + Camera test

--- PlayerController.gd
+++ PlayerController.gd
@@ -8,6 +8,7 @@ extends Node2D
 var currchar = []
 var numchar = 0
 @export var currnumchar = 0
+var cam
 
 # Called when the node enters the scene tree for the first time.
 
@@ -18,11 +19,13 @@ func _ready():
 		print(i.selection)
 		numchar += 1
 	currchar[currnumchar].selected = true
+	currchar[currnumchar].get_node("Camera2D").make_current()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.	
 func _input(ev):
 	if ev.is_action_pressed("changechar"):
 		currchar[currnumchar].selected = false
+		var cam1 = currchar[currnumchar].get_node("Camera2D")
 		print("Changed character")
 		if currnumchar == numchar-1:
 			currnumchar = 0
@@ -30,3 +33,10 @@ func _input(ev):
 			currnumchar += 1
 		
 		currchar[currnumchar].selected = true
+		var cam2 = currchar[currnumchar].get_node("Camera2D")
+		TweenCam(cam1, cam2)
+		currchar[currnumchar].get_node("Camera2D").make_current()
+		
+func TweenCam(cam1, cam2):
+	pass
+	#TWEEN FUNCTION HERE
