cf3ac2b923c16d9105748a17c18f4061ac0e9178
samokani <jonsimonpasana@gmail.com>
2023-07-08 17:45:03 +0800

    Updated camera TEST

--- PlayerController.gd
+++ PlayerController.gd
@@ -7,8 +7,11 @@ extends Node2D
 
 var currchar = []
 var numchar = 0
+
+@onready var cam = $Camera
+var lerpspeed = .1
+
 @export var currnumchar = 0
-var cam
 
 # Called when the node enters the scene tree for the first time.
 
@@ -19,24 +22,22 @@ func _ready():
 		print(i.selection)
 		numchar += 1
 	currchar[currnumchar].selected = true
-	currchar[currnumchar].get_node("Camera2D").make_current()
+	cam.reparent(currchar[currnumchar])
+	cam.position = Vector2(0, 0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(ev):
 	if ev.is_action_pressed("changechar"):
 		currchar[currnumchar].selected = false
-		var cam1 = currchar[currnumchar].get_node("Camera2D")
 		print("Changed character")
 		if currnumchar == numchar-1:
 			currnumchar = 0
 		else:
 			currnumchar += 1
-		
 		currchar[currnumchar].selected = true
-		var cam2 = currchar[currnumchar].get_node("Camera2D")
-		TweenCam(cam1, cam2)
-		currchar[currnumchar].get_node("Camera2D").make_current()
-		
-func TweenCam(cam1, cam2):
-	pass
-	#TWEEN FUNCTION HERE
+		cam.reparent(currchar[currnumchar])
+
+# Called every physics frame. Used to handle camera follow and tweening.
+func _physics_process(delta):
+	cam.position = lerp(cam.position, Vector2(0, 0), lerpspeed)
+
