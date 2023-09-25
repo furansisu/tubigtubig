708384c3ae41f9ef6093a41e7ee8fe8ac3e5bc8c
WIN10X64-TRIAL <francispasana@gmail.com>
2023-09-25 17:57:08 +0800

    Initial Context-steering functions

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,25 +12,84 @@ var direction = Vector2.ZERO
 @export var target_position = position
 @export var MovingTo = false
 
-var Marker
+var Marker 
 var Force
 var Moving = false
-var ray = RayCast2D.new()
+
+## CONTEXT STEERING -------------------------------------------------------------------
+
+@export var num_rays = 64
+
+var ray_length = 25
+var ray_directions = []
+var interest = []
+var danger = []
+
+var debug_dirs = []
+var debug_danger = []
+var prefDir = Vector2.ZERO
+
+func debugRays():
+	var space_state = get_world_2d().direct_space_state
+	for i in num_rays:
+		var query = PhysicsRayQueryParameters2D.create(position, position+ray_directions[i].rotated(rotation) * ray_length, 1, [self])
+		var result = space_state.intersect_ray(query)
+		danger[i] = 1 if result else 0
+		
+		debug_dirs[i*2] = Vector2.ZERO
+		debug_dirs[i*2+1] = ray_directions[i] * ray_length * interest[i]
+		
+		debug_danger[i*2] = Vector2.ZERO
+		debug_danger[i*2+1] = ray_directions[i] * ray_length * danger[i]
+
+func getInterest():
+	var dir = position.direction_to(target_position)
+	for i in num_rays:
+		var d = ray_directions[i].rotated(rotation).dot(dir)
+		interest[i] = max(0, d)
+	queue_redraw()
+		
+func chooseDir():
+	prefDir = Vector2.ZERO
+	for i in num_rays:
+		interest[i] = clamp(interest[i] - danger[i], 0, 1)
+		prefDir += ray_directions[i] * interest[i]
+	prefDir = prefDir.normalized()
+	print(prefDir)
+	
+
+func _draw():
+	draw_line(Vector2.ZERO, Vector2.ZERO + prefDir.rotated(rotation) * 50, Color.GREEN_YELLOW, 2)
+	draw_multiline(debug_dirs, Color.YELLOW_GREEN, 0.75)
+	draw_multiline(debug_danger, Color.RED, 1)
+
+## -----------------------------------------------------------------------------------
 
 func _ready():
-	Marker = get_node("../../Marker")
-	Force = get_node("../../Force")
-	self.add_child(ray)
-	print(Force.global_position)
+	ray_directions.resize(num_rays)
+	interest.resize(num_rays)
+	danger.resize(num_rays)
+	debug_dirs.resize(num_rays*2)
+	debug_danger.resize(num_rays*2)
+	for i in num_rays:
+		var angle = i * 2 * PI / num_rays
+		ray_directions[i] = Vector2.RIGHT.rotated(angle)
+		debug_dirs[i*2] = Vector2.ZERO
+		debug_dirs[i*2+1] = ray_directions[i] * ray_length
 	
+	Marker = get_node("../../Marker")
+
+# UNUSED --------------------------------------------------------------------------------
+
+func force():
+	pass
+
+# UNUSED --------------------------------------------------------------------------------
 
 func MoveTo(Position: Vector2, StopMoving):
 	Moving = StopMoving
 	Marker.position = Position
 	target_position = Position
-	
-func force():
-	ray.target_position = Force.global_position - self.global_position
 
 func ChangeMotion(newDir):
 	direction = newDir
@@ -38,13 +97,17 @@ func ChangeMotion(newDir):
 		Moving = false
 
 func _physics_process(delta):
-	force()
+	#force()
+	getInterest()
+	debugRays()
+	chooseDir()
+	
 	if Moving:
 		if position.distance_to(target_position) < 5:
 			target_position = position
 			print("Reached target")
 			Moving = false
-		direction = position.direction_to(target_position)
+		direction = prefDir
 	
 	# animation handling
 	if (direction != Vector2.ZERO and (velocity > Vector2.ZERO or velocity < Vector2.ZERO)):
