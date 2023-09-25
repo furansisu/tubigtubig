1d04e42c7d5f84c6c928d88969658fc625fa4604
WIN10X64-TRIAL <francispasana@gmail.com>
2023-09-25 20:42:18 +0800

    Updated CSB functions

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -18,7 +18,7 @@ var Moving = false
 
 ## CONTEXT STEERING -------------------------------------------------------------------
 
-@export var num_rays = 64
+@export var num_rays = 32
 
 var ray_length = 25
 var ray_directions = []
@@ -26,42 +26,48 @@ var interest = []
 var danger = []
 
 var debug_dirs = []
-var debug_danger = []
+var colors = []
 var prefDir = Vector2.ZERO
 
-func debugRays():
+func getDanger():
 	var space_state = get_world_2d().direct_space_state
 	for i in num_rays:
 		var query = PhysicsRayQueryParameters2D.create(position, position+ray_directions[i].rotated(rotation) * ray_length, 1, [self])
 		var result = space_state.intersect_ray(query)
-		danger[i] = 1 if result else 0
+		danger[i] = clamp(1 - (position.distance_to(result.position) / ray_length),0,1) if result else 0
+		colors[i] = Color.RED if result else Color.YELLOW_GREEN
 		
 		debug_dirs[i*2] = Vector2.ZERO
-		debug_dirs[i*2+1] = ray_directions[i] * ray_length * interest[i]
-		
-		debug_danger[i*2] = Vector2.ZERO
-		debug_danger[i*2+1] = ray_directions[i] * ray_length * danger[i]
+		debug_dirs[i*2+1] = ray_directions[i] * ray_length
+		debug_dirs[i*2+1] *= danger[i] if result else interest[i]
 
 func getInterest():
 	var dir = position.direction_to(target_position)
 	for i in num_rays:
 		var d = ray_directions[i].rotated(rotation).dot(dir)
+		var opp = getOpposite(i)
+		if danger[opp]:
+			d = ray_directions[i].rotated(rotation).dot(ray_directions[i]) * danger[opp] * 2
 		interest[i] = max(0, d)
 	queue_redraw()
 		
 func chooseDir():
 	prefDir = Vector2.ZERO
 	for i in num_rays:
-		interest[i] = clamp(interest[i] - danger[i], 0, 1)
+		interest[i] = interest[i] - danger[i]
 		prefDir += ray_directions[i] * interest[i]
 	prefDir = prefDir.normalized()
 	print(prefDir)
-	
+
+func getOpposite(i):
+	if i < num_rays / 2:
+		return i + num_rays / 2
+	else:
+		return i - num_rays / 2
 
 func _draw():
 	draw_line(Vector2.ZERO, Vector2.ZERO + prefDir.rotated(rotation) * 50, Color.GREEN_YELLOW, 2)
-	draw_multiline(debug_dirs, Color.YELLOW_GREEN, 0.75)
-	draw_multiline(debug_danger, Color.RED, 1)
+	draw_multiline_colors(debug_dirs, colors, 0.75)
 
 ## -----------------------------------------------------------------------------------
 
@@ -70,7 +76,7 @@ func _ready():
 	interest.resize(num_rays)
 	danger.resize(num_rays)
 	debug_dirs.resize(num_rays*2)
-	debug_danger.resize(num_rays*2)
+	colors.resize(num_rays)
 	for i in num_rays:
 		var angle = i * 2 * PI / num_rays
 		ray_directions[i] = Vector2.RIGHT.rotated(angle)
@@ -99,7 +105,7 @@ func ChangeMotion(newDir):
 func _physics_process(delta):
 	#force()
 	getInterest()
-	debugRays()
+	getDanger()
 	chooseDir()
 	
 	if Moving:
