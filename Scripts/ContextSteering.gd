77f90a9992ae31aec8ea505731f1cde768d1f713
WIN10X64-TRIAL <francispasana@gmail.com>
2023-09-28 11:16:11 +0800

    Externalized CSB functions into separate script + mini documentation

---
+++ Scripts/ContextSteering.gd
@@ -0,0 +1,230 @@
+extends Node
+@onready var default_font = load("res://Resources/FONTS/november.tres")
+
+# ------------------------ V A R I A B L E S ------------------------------------------
+# IMPORTANT SETTINGS HERE
+
+var debug = true
+
+var WEIGHTS = {
+	"CharacterBody2D": 10,
+	"TileMap": 1.25,
+	"Area2D": 10
+}
+
+var num_rays = 16
+var ray_length = 15
+
+# ------------------------
+# IMPORTANT ARRAYS HERE
+
+var rays = []
+# contains all ray direction vectors around a character. Number is based on num_rays and is set up in _ready function
+
+var interest = []
+var danger = []
+
+# both interest and danger arrays correspond to a direction in the rays array (they share the same index)
+
+var InterestDirWeights = {}
+var DangerDirWeights = {}
+# currently detected interest directions and danger directions. These are vital in calculating the
+# 	interest and danger arrays.
+# [direction, result_position, weight_of_detected_object]
+
+var collisions = {}
+# {RAY_INDEX : collision_object}
+
+var colors = []
+# holds colors for the lines in debug
+
+# ------------------------------------------------------------------------------------
+# MAIN FUNCTIONS
+# meat of context steering behavior
+# ------------------------------------------------------------------------------------
+
+var char : CharacterBody2D
+func setup(character : CharacterBody2D):
+	char = character
+	rays.resize(num_rays)
+	interest.resize(num_rays)
+	danger.resize(num_rays)
+	debug_dirs.resize(num_rays*2)
+	colors.resize(num_rays)
+	for i in num_rays:
+		var angle = i * 2 * PI / num_rays
+		rays[i] = Vector2.RIGHT.rotated(angle)
+		debug_dirs[i*2] = Vector2.ZERO
+		debug_dirs[i*2+1] = rays[i] * ray_length
+		
+# Sets up context steering for a character.
+# Resizes and sets up all needed arrays and creates all the directions for raycasting around a character and
+# 	saves them into the rays array.
+
+# --------------------------------------
+var prevMaxInterests : Array = []
+func chooseDir():
+	getInterest()
+	getDanger()
+	debugCollision()
+	for i in num_rays:
+		interest[i] = interest[i] - danger[i]
+		#prefDir += ray_directions[i] * interest[i]
+		
+		# DEBUG
+		debug_dirs[i*2] = Vector2.ZERO
+		debug_dirs[i*2+1] = rays[i] * ray_length
+		if interest[i] > 0:
+			debug_dirs[i*2+1] *= interest[i]
+			colors[i] = Color.YELLOW_GREEN
+		else:
+			debug_dirs[i*2+1] *= abs(interest[i])
+			colors[i] = Color.RED
+		# DEBUG
+		
+	var indexes = findAllHighest(interest)
+	var index
+	if prevMaxInterests.is_empty():
+		prevMaxInterests = savePrevMax(interest, indexes)
+	#print("GETTING ALL MAX INTERESTS AND COMPARING WITH CURRENT VELOCITY")
+	var minimum = 100
+	var minIndex = 0
+	for i in indexes:
+		var magnitude = (rays[i] - char.velocity.normalized()).length()
+		if magnitude < 0.01:
+			index = i
+		if magnitude < minimum:
+			minimum = magnitude
+			minIndex = i
+		#print(i, " magnitude: ", magnitude)
+	#print("CHOSEN: ", index)
+	if not index:
+		index = minIndex
+	#print(indexes)
+	var prefDir = rays[index]
+	prevMaxInterests = savePrevMax(interest, indexes)
+	return prefDir
+
+# --------------------------------------
+func getDanger():
+	var space_state = char.get_world_2d().direct_space_state
+	
+	# FIRST LOOP: Raycast and detect any objects within range.
+	# 	For each ray with a result, save the direction into DangerDirWeights
+	for i in num_rays:
+		var query = PhysicsRayQueryParameters2D.create(char.position, char.position+rays[i].rotated(char.rotation) * ray_length, 1, [char])
+		query.collide_with_areas = true
+		var result = space_state.intersect_ray(query)
+		if result:
+			DangerDirWeights[i] = ([rays[i], result.position, getWeight(result.collider.get_class())])
+			collisions[i] = result
+		else:
+			collisions.erase(i)
+			DangerDirWeights.erase(i)
+			
+	# SECOND LOOP: For each ray direction, calculate the danger weight based on each DangerDirWeight.
+	# 	Uses dot product for how far the ray is to the direction and is multiplied by how far the character is to
+	# 	the object.
+	for i in num_rays:
+		var all = 0
+		for j in DangerDirWeights:
+			var d = rays[i].dot(DangerDirWeights[j][0]) * clamp(1 - (char.position.distance_to(DangerDirWeights[j][1]) / ray_length),0,1) * DangerDirWeights[j][2]
+			all += d
+		all = clamp(all, 0, 1)
+		danger[i] = max(0, all)
+		colors[i] = Color.RED if danger[i] > 0 else Color.YELLOW_GREEN
+
+# --------------------------------------
+func getInterest():
+	var dir = char.position.direction_to(char.target_position)
+	for i in num_rays:
+		var d = rays[i].dot(dir)
+		#var d = 1 - abs(ray_directions[i].dot(dir) - 0.2)
+		var opp = getOpposite(i)
+		if danger[opp]:
+			d = max(d, danger[opp] * 0.25)
+			#d = max(d, 1 - abs(ray_directions[i].dot(-dir) - 0.5))
+			pass
+		interest[i] = max(0, d)
+	char.queue_redraw()
+
+# ------------------------------------------------------------------------------------
+# DEBUG FUNCTIONS
+# will only work if debug is set to true. Shows raycasts and collisions
+# ------------------------------------------------------------------------------------
+
+var debugText : Array = []
+# [["TEXT", Color, Vector2, Collider], ["COLLIDE", Color, Vector2, TileMap]]
+
+var debug_dirs : Array = []
+# [DIRECTION, POSITION, WEIGHT]
+
+func _draw():
+	if not debug:
+		return
+	char.draw_line(Vector2.ZERO, Vector2.ZERO + char.prefDir.rotated(char.rotation) * 50, Color.GREEN_YELLOW, 2)
+	char.draw_multiline_colors(debug_dirs, colors, 0.75)
+	for i in collisions:
+		char.draw_circle(collisions[i].position - char.global_position,1, Color.RED)
+	for i in debugText:
+		var size = 5
+		char.draw_string(default_font, i[2] - char.global_position - Vector2(size/2,0), i[0], HORIZONTAL_ALIGNMENT_LEFT, -1, size, i[1])
+		await char.get_tree().create_timer(1).timeout
+		debugText.erase(i)
+
+func debugCollision():
+	for i in char.get_slide_collision_count():
+		var collision = char.get_slide_collision(i)
+		var alreadyCollided = false
+		for j in debugText:
+			if j[3] and j[3] == collision.get_collider():
+				alreadyCollided = true
+				break
+		if not alreadyCollided:
+			var newCollision = ["collide", Color.RED, collision.get_position(), collision.get_collider()]
+			debugText.append(newCollision)
+		
+# ------------------------------------------------------------------------------------
+# MINI FUNCTIONS
+# these functions do specific tasks for other functions
+# ------------------------------------------------------------------------------------
+
+func savePrevMax(array: Array, indexes: Array):
+	var newArray = []
+	for i in indexes:
+		newArray.append([array[i],i])
+	return newArray
+
+# Returns an array containing the previous frame's max (highest) interest directions and indexes.
+# [direction : Vector2, index_in_rays_array : int]
+
+# --------------------------------------
+var threshold = 0.1
+func findAllHighest(array : Array):
+	var maxvalue = array.max()
+	var indexes = []
+	var index = 0
+	for i in array:
+		if abs(i - maxvalue) <= threshold:
+			indexes.append(index)
+		index += 1
+	return indexes
+
+# Loops through an array and returns an array holding the highest values within a threshold.
+
+# --------------------------------------
+func getWeight(collider):
+	var result = WEIGHTS.get(collider)
+	return result if result else 0.5
+
+# Returns the weight of a detected object from a raycast. Weight is how dangerous and repellant an object is.
+# Refer to WEIGHTS dictionary.
+
+# --------------------------------------
+func getOpposite(i):
+	if i < num_rays / 2:
+		return i + (num_rays / 2)
+	else:
+		return i - (num_rays / 2)
+
+# Given the index of a vector in the rays array, returns the index of the opposing vector.
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,183 +1,23 @@
 extends CharacterBody2D
+const contextsteeringscript = preload("res://Scripts/ContextSteering.gd")
+@onready var CSB = contextsteeringscript.new()
 
-# Called when the node enters the scene tree for the first time.
+# ------------------------ V A R I A B L E S ------------------------------------------
 
-@export var default_acc = 100
-var acc = default_acc
-@export var maxspeed = 100
+var maxspd = 65
+var default_acc = maxspd
 @export var selection = 0
-var friction = (acc/maxspeed)
 
 var direction = Vector2.ZERO
 @export var target_position = position
 @export var MovingTo = false
 
-var Marker 
-var Force
-var Moving = false
-
-@onready var default_font = load("res://Resources/FONTS/november.tres")
-
-## CONTEXT STEERING -------------------------------------------------------------------
-
-var WEIGHTS = {
-	"CharacterBody2D": 10,
-	"TileMap": 1.25,
-	"Area2D": 5
-}
-
-@export var num_rays = 16
-
-var ray_length = 15
-var ray_directions = []
-var interest = []
-var danger = []
-
-var InterestDirWeights = {}
-var DangerDirWeights = {}
-
-# [DIRECTION, POSITION, WEIGHT]
-
-var debug_dirs = []
-var colors = []
 var prefDir = Vector2.ZERO
 
-func getDanger():
-	var space_state = get_world_2d().direct_space_state
-	for i in num_rays:
-		var query = PhysicsRayQueryParameters2D.create(position, position+ray_directions[i].rotated(rotation) * ray_length, 1, [self])
-		query.collide_with_areas = true
-		var result = space_state.intersect_ray(query)
-		
-		if result:
-			DangerDirWeights[i] = ([ray_directions[i], result.position, getWeight(result.collider.get_class())])
-			#danger[i] = clamp(1 - (position.distance_to(result.position) / ray_length),0,1)
-			
-		else:
-			DangerDirWeights.erase(i)
-			#danger[i] = 0
-	
-	for i in num_rays:
-		var all = 0
-		for j in DangerDirWeights:
-			var d = ray_directions[i].dot(DangerDirWeights[j][0]) * clamp(1 - (position.distance_to(DangerDirWeights[j][1]) / ray_length),0,1) * DangerDirWeights[j][2]
-			all += d
-		all = clamp(all, 0, 1)
-		danger[i] = max(0, all)
-		colors[i] = Color.RED if danger[i] > 0 else Color.YELLOW_GREEN
+# ------------------------------------------------------------------------------------
 
-func getInterest():
-	var dir = position.direction_to(target_position)
-	for i in num_rays:
-		var d = ray_directions[i].dot(dir)
-		#var d = 1 - abs(ray_directions[i].dot(dir) - 0.17)
-		var opp = getOpposite(i)
-		if danger[opp]:
-			d = max(d, danger[opp] * 0.25)
-			#d = max(d, 1 - abs(ray_directions[i].dot(-dir) - 0.5))
-			pass
-		interest[i] = max(0, d)
-	queue_redraw()
-
-var prevMaxInterests : Array = []
-func chooseDir():
-	prefDir = Vector2.ZERO
-	for i in num_rays:
-		interest[i] = interest[i] - danger[i]
-		#prefDir += ray_directions[i] * interest[i]
-		
-		# DEBUG
-		debug_dirs[i*2] = Vector2.ZERO
-		debug_dirs[i*2+1] = ray_directions[i] * ray_length
-		if interest[i] > 0:
-			debug_dirs[i*2+1] *= interest[i]
-			colors[i] = Color.YELLOW_GREEN
-		else:
-			debug_dirs[i*2+1] *= abs(interest[i])
-			colors[i] = Color.RED
-		# DEBUG
-		
-	var indexes = findAll(interest, interest.max())
-	var index
-	if prevMaxInterests.is_empty():
-		prevMaxInterests = savePrevMax(interest, indexes)
-	print("GETTING ALL MAX INTERESTS AND COMPARING WITH CURRENT VELOCITY")
-	var minimum = 100
-	var minIndex = 0
-	for i in indexes:
-		var magnitude = (ray_directions[i] - velocity.normalized()).length()
-		if magnitude < 1:
-			index = i
-		if magnitude < minimum:
-			minimum = magnitude
-			minIndex = i
-		print(i, " magnitude: ", magnitude)
-	print("CHOSEN: ", index)
-	if not index:
-		index = minIndex
-	print(indexes)
-	prefDir = ray_directions[index]
-	prevMaxInterests = savePrevMax(interest, indexes)
-
-func savePrevMax(array: Array, indexes: Array):
-	var newArray = []
-	for i in indexes:
-		newArray.append([array[i],i])
-	return newArray
-
-func getWeight(collider):
-	var result = WEIGHTS.get(collider)
-	return result if result else 0.5
-
-func getOpposite(i):
-	if i < num_rays / 2:
-		return i + (num_rays / 2)
-	else:
-		return i - (num_rays / 2)
-
-var debugText : Array = []
-# [["TEXT", Color, Vector2, Collider], ["COLLIDE", Color, Vector2, TileMap]]
-
-func drawText(i):
-	var size = 5
-	draw_string(default_font, i[2] - self.global_position - Vector2(size/2,0), i[0], HORIZONTAL_ALIGNMENT_LEFT, -1, size, i[1])
-	await get_tree().create_timer(1).timeout
-	debugText.erase(i)
-
-func _draw():
-	draw_line(Vector2.ZERO, Vector2.ZERO + prefDir.rotated(rotation) * 50, Color.GREEN_YELLOW, 2)
-	draw_multiline_colors(debug_dirs, colors, 0.75)
-	for i in debugText:
-		drawText(i)
-
-## -----------------------------------------------------------------------------------
-
-func _ready():
-	ray_directions.resize(num_rays)
-	interest.resize(num_rays)
-	danger.resize(num_rays)
-	debug_dirs.resize(num_rays*2)
-	colors.resize(num_rays)
-	for i in num_rays:
-		var angle = i * 2 * PI / num_rays
-		ray_directions[i] = Vector2.RIGHT.rotated(angle)
-		debug_dirs[i*2] = Vector2.ZERO
-		debug_dirs[i*2+1] = ray_directions[i] * ray_length
-	
-	Marker = get_node("../../Marker")
-
-# OTHER --------------------------------------------------------------------------------
-
-func findAll(array : Array, value):
-	var indexes = []
-	var index = 0
-	for i in array:
-		if abs(i - value) <= 0.1:
-			indexes.append(index)
-		index += 1
-	return indexes
-
-# OTHER --------------------------------------------------------------------------------
+var Marker 
+var Moving = false
 
 func MoveTo(Position: Vector2, StopMoving):
 	Moving = StopMoving
@@ -189,23 +29,17 @@ func ChangeMotion(newDir):
 	if newDir.length() > .1:
 		Moving = false
 
+# ------------------------------------------------------------------------------------
+
+func _draw():
+	CSB._draw()
+
+func _ready():
+	CSB.setup(self)
+	Marker = get_node("../../Marker")
+
 func _physics_process(delta):
-	#force()
-	getInterest()
-	getDanger()
-	chooseDir()
-	
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var alreadyCollided = false
-		for j in debugText:
-			if j[3] and j[3] == collision.get_collider():
-				alreadyCollided = true
-				break
-		if not alreadyCollided:
-			var newCollision = ["collide", Color.RED, collision.get_position(), collision.get_collider()]
-			debugText.append(newCollision)
-		
+	var prefDir = CSB.chooseDir()
 	
 	if Moving:
 		if position.distance_to(target_position) < 5:
@@ -220,25 +54,23 @@ func _physics_process(delta):
 			$AnimationPlayer.play("Run")
 		else:
 			$AnimationPlayer.play("WalkRight")
-		if direction.x < 0:
+		#if direction.x < 0:
+		if (position - target_position).x > 0:
 			$Sprite2D.flip_h = true
-		if direction.x > 0:
+		#if direction.x > 0:
+		if (position - target_position).x < 0:
 			$Sprite2D.flip_h = false
 	else:
 		$AnimationPlayer.play("Idle")
 	
-	var desired_velocity = direction * maxspeed
-	velocity = velocity.lerp(desired_velocity, 0.125)
-	#velocity += direction * acc * delta
-	#velocity -= velocity * friction * delta
+	var desired_velocity = direction * maxspd
+	velocity = velocity.lerp(desired_velocity, 0.15)
 		
 	# run test
 	if Input.is_action_pressed("run"):
-		maxspeed = default_acc * 1.5
+		maxspd = default_acc * 1.5
 	else:
-		maxspeed = default_acc
-	
-	# actual moving
+		maxspd = default_acc
 	
 	move_and_slide()
 	
