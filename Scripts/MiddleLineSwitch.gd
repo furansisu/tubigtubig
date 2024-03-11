c2ccb9fd1a65b97aec7aa0d63721c2d2b9f072d7
フランシス <74588331+furansisu@users.noreply.github.com>
2024-03-12 07:41:49 +0800

    ACTUAL joystick, touch buttons

--- Scripts/MiddleLineSwitch.gd
+++ Scripts/MiddleLineSwitch.gd
@@ -2,28 +2,28 @@ extends Area2D
 
 func Entered(_person):
 #	print("SOMEONE ENTERED")
-	pass
-	
+    pass
+    
 func Exited(person):
 #	print("SOMEONE EXITED")
-	if person.currentTeam != "Taggers":
-		return
-	if person.global_position.y > self.global_position.y:
+    if person.currentTeam != "Taggers":
+        return
+    if person.global_position.y > self.global_position.y:
 #		print(person.name + " is in ORIGINAL LINE")
-		person.middleLine = false
-	else:
+        person.middleLine = false
+    else:
 #		print(person.name + " is in MIDDLE LINE")
-		person.middleLine = true
+        person.middleLine = true
 
 @onready var collider : CollisionShape2D = get_node("CollisionShape2D")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if not collider:
-		push_error("ERROR: Area has no collision detection!")
-	body_entered.connect(Entered)
-	body_exited.connect(Exited)
+    if not collider:
+        push_error("ERROR: Area has no collision detection!")
+    body_entered.connect(Entered)
+    body_exited.connect(Exited)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+    pass
--- Scripts/States/Strafe.gd
+++ Scripts/States/Strafe.gd
@@ -13,103 +13,103 @@ var sideLook = false
 var players : Array
 
 func randomize_wander():
-	dir = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
-	wander_time = randf_range(1,2)
-	
+    dir = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
+    wander_time = randf_range(1,2)
+    
 func random_newpos():
-	pos = Vector2(randi_range(-100,100), randi_range(-100,100)) + character.global_position
-	wander_time = randf_range(6,10)
+    pos = Vector2(randi_range(-100,100), randi_range(-100,100)) + character.global_position
+    wander_time = randf_range(6,10)
 
 var prevPos = Vector2(0,0)
 func random_pos_area():
 #	print(character.name + ": GETTING RANDOM POS IN " + character.targetArea.name)
-	if not character.targetArea:
-		changeToNextArea()
-	pos = AreaHandler.random_pos(character.targetArea)
+    if not character.targetArea:
+        changeToNextArea()
+    pos = AreaHandler.random_pos(character.targetArea)
 #	print(character.name + ": NEW POSITION: " + str(pos) + " FROM " + str(prevPos))
-	prevPos = pos
-	wander_time = randf_range(1,2)
-	
+    prevPos = pos
+    wander_time = randf_range(1,2)
+    
 func changeToNextArea():
-	character.targetArea = AreaHandler.getNextAreaOfCharacter(character)
-	sideLook = false
+    character.targetArea = AreaHandler.getNextAreaOfCharacter(character)
+    sideLook = false
 
 func changeToSideArea():
-	var sideArea = AreaHandler.getSideAreaOfCharacter(character)
+    var sideArea = AreaHandler.getSideAreaOfCharacter(character)
 #	print("CHANGING AREA FOR " + character.name + " FROM " + character.targetArea.name + " TO " + sideArea.name)
-	character.targetArea = sideArea
-	sideLook = true
-	character.movingToSide = true
-	random_pos_area()
-	
+    character.targetArea = sideArea
+    sideLook = true
+    character.movingToSide = true
+    random_pos_area()
+    
 func checkLowestDistanceToTagger():
-	var distances : Dictionary = {}
-	var lowestDist = 9999
-	
-	if sideLook == true:
-		for i in players:
-			if i.middleLine == true:
-				return i.global_position.distance_to(character.global_position)
-		wander_time = 0
-		return 9999
-	
-	for i in players:
-		distances[i.name] = i.global_position.distance_to(character.global_position)
-	
-	for target in distances:
-		if distances[target] <= lowestDist:
-			lowestDist = distances[target]
-	
-	character.distanceToClosestTagger = lowestDist
-	return lowestDist
+    var distances : Dictionary = {}
+    var lowestDist = 9999
+    
+    if sideLook == true:
+        for i in players:
+            if i.middleLine == true:
+                return i.global_position.distance_to(character.global_position)
+        wander_time = 0
+        return 9999
+    
+    for i in players:
+        distances[i.name] = i.global_position.distance_to(character.global_position)
+    
+    for target in distances:
+        if distances[target] <= lowestDist:
+            lowestDist = distances[target]
+    
+    character.distanceToClosestTagger = lowestDist
+    return lowestDist
 
 func checkTeammateGoingSide():
-	var sideArea = AreaHandler.getSideAreaOfCharacter(character)
-	var runners = get_node("/root/World").RunnersOnField
-	for player in runners:
-		if player.currentArea == sideArea or player.targetArea == sideArea:
-			print("A TEAMMATE IS GOING TO THE SIDE!")
-			return true
-	return false
+    var sideArea = AreaHandler.getSideAreaOfCharacter(character)
+    var runners = get_node("/root/World").RunnersOnField
+    for player in runners:
+        if player.currentArea == sideArea or player.targetArea == sideArea:
+            print("A TEAMMATE IS GOING TO THE SIDE!")
+            return true
+    return false
 
 func Enter():
-	if not character:
-		character = get_parent().get_parent()
-	if not AreaHandler.checkIfInArea(character):
-			Transitioned.emit(self, "Forward")
-	character.movingToSide = false
-	random_pos_area()
-	changeToNextArea()
-	character.DisableAreaRays.emit(false)
-	character.DisablePlayerRays.emit(false)
-	character.DisableBorderRays.emit(false)
-	character.ReachedTarget.connect(random_pos_area)
-	players = get_node("/root/World").Taggers
+    if not character:
+        character = get_parent().get_parent()
+    if not AreaHandler.checkIfInArea(character):
+            Transitioned.emit(self, "Forward")
+    character.movingToSide = false
+    random_pos_area()
+    changeToNextArea()
+    character.DisableAreaRays.emit(false)
+    character.DisablePlayerRays.emit(false)
+    character.DisableBorderRays.emit(false)
+    character.ReachedTarget.connect(random_pos_area)
+    players = get_node("/root/World").Taggers
 
 func Exit():
-	if character.ReachedTarget.is_connected(random_pos_area):
-		character.ReachedTarget.disconnect(random_pos_area)
-	
+    if character.ReachedTarget.is_connected(random_pos_area):
+        character.ReachedTarget.disconnect(random_pos_area)
+    
 func Update(delta):
-	if character.currentArea == character.targetArea:
-		changeToNextArea()
-	
-	var lowestDist = checkLowestDistanceToTagger()
-	if lowestDist > 35:
-		wander_time = 0
-	if wander_time > 0:
-		wander_time -= delta
-	else:
-		if lowestDist <= 35 and sideLook == false and not checkTeammateGoingSide():
-			changeToSideArea()
+    if character.currentArea == character.targetArea:
+        changeToNextArea()
+    
+    var lowestDist = checkLowestDistanceToTagger()
+    if lowestDist > 35:
+        wander_time = 0
+    if wander_time > 0:
+        wander_time -= delta
+    else:
+        if lowestDist <= 35 and sideLook == false and not checkTeammateGoingSide():
+            changeToSideArea()
 #			print("Moving to other side for " + character.name)
-		else:
-			if sideLook == false:
-				changeToNextArea()
+        else:
+            if sideLook == false:
+                changeToNextArea()
 #			print("Moving FORWARD for " + character.name + ", since lowestDist = " + str(lowestDist))
 #			print("GOING TO THE SIDE? " + str(sideLook) + " towards " + character.targetArea.name)
-			Transitioned.emit(self, "Forward")
-		
+            Transitioned.emit(self, "Forward")
+        
 func Physics_Update(_delta):
-	if character:
-		character.MoveTo(pos, 15)
+    if character:
+        character.MoveTo(pos, 15)
